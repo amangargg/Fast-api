6bb07c15d52ef2b4178d40dfb25be9ed3e84400b
amangargg <amangarg@Amans-MacBook-Air-3.local>
2026-06-17 00:51:25 +0530

    login  done

--- app/oauth2.py
+++ app/oauth2.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import Session
 import os
 
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl='signin')
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 #SECREAT KEY
 #ALGORITHM
 #EXPRIATION TIME
