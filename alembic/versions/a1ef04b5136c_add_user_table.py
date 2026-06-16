ef4ff26cd40b49affbfa32f2a8a4206a67a8cb33
amangargg <amangarg@Amans-MacBook-Air-3.local>
2026-06-16 23:05:29 +0530

    finally

--- alembic/versions/a1ef04b5136c_add_user_table.py
+++ alembic/versions/a1ef04b5136c_add_user_table.py
@@ -19,7 +19,7 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    op.create_table(' users',
+    op.create_table('users',
                     sa.Column ('id', sa. Integer(), nullable=False),
                     sa.Column ('email', sa.String(), nullable=False),
                     sa.Column ('password', sa. String(), nullable=False),
